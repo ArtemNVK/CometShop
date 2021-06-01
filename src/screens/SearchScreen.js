cc61ca75cf63916c95594cc148cc7a735f382cf7
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-06-01 23:30:02 +0300

    Make search screen filters and sort responsive

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,7 +8,9 @@ import Product from '../components/Product';
 import Rating from '../components/Rating';
 import { prices, ratings } from '../utils';
 import Pagination from '../components/Pagination';
+import { BsFilterLeft } from 'react-icons/bs';
 
+ 
 export default function SearchScreen(props) {
   document.title = "Search"
   const {
@@ -29,7 +31,7 @@ export default function SearchScreen(props) {
     categories,
   } = productCategoryList;
   const [page, setPage] = useState(1);
-
+  const [isFiltersOpen, setIsFiltersOpen] = useState(false);
 
 
   useEffect(() => {
@@ -56,7 +58,7 @@ export default function SearchScreen(props) {
   };
 
   return (
-    <div>
+    <div className="search-screen-container">
       <div className="row top">
         <div className="col-1 categoriesAndFilters">
           <h3>Department</h3>
@@ -123,6 +125,94 @@ export default function SearchScreen(props) {
             </ul>
           </div>
         </div>
+
+                {/* Smallscreens filters */}
+        <div className="smallscreens-filterBtnSort-container">
+          <button className="smallscreens-filters-btn" onClick={() => setIsFiltersOpen(true)}><BsFilterLeft></BsFilterLeft></button>
+          <div className="smallscreens-sortBy-container">
+            <select
+              value={order}
+              onChange={(e) => {
+                props.history.push(getFilterUrl({ order: e.target.value }));
+              }}
+            >
+              <option value="newest">Newest Arrivals</option>
+              <option value="lowest">Price: Low to High</option>
+              <option value="highest">Price: High to Low</option>
+              <option value="toprated">Avg. Customer Reviews</option>
+            </select>
+          </div>
+        </div>
+        <div className={isFiltersOpen ? "smallscreens-searchscreen-filters-container active" : "smallscreens-searchscreen-filters-container"}>      
+          <div className="smallscreens-searchscreen-filters">
+            <button className="smallscreens-searchscreen-filters-close" onClick={() => setIsFiltersOpen(false)}>Close</button>
+            <h3>Department</h3>
+            <div>
+              {loadingCategories ? (
+                <LoadingBox></LoadingBox>
+              ) : errorCategories ? (
+                <MessageBox variant="danger">{errorCategories}</MessageBox>
+              ) : (
+                <ul>
+                  <li>
+                    <Link
+                      className={'all' === category ? 'active' : ''}
+                      to={getFilterUrl({ category: 'all' })}
+                    >
+                      All
+                    </Link>
+                  </li>
+                  {categories.map((c) => (
+                    <li key={c}>
+                      <Link
+                        className={c === category ? 'active' : ''}
+                        to={getFilterUrl({ category: c })}
+                      >
+                        {c}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+            <div>
+              <h3>Price</h3>
+              <ul>
+                {prices.map((p) => (
+                  <li key={p.name}>
+                    <Link
+                      to={getFilterUrl( `${p.min}-${p.max}` === `${min}-${max}` ? { min: '0', max: '0'} : { min: p.min, max: p.max })}
+                      className={
+                        `${p.min}-${p.max}` === `${min}-${max}` ? 'active' : ''
+                      }
+                    >
+                      {p.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <h3>Avg. Customer Review</h3>
+              <ul>
+                {ratings.map((r) => (
+
+                  <li key={r.name}>
+                    <Link
+                      to={getFilterUrl(`${r.rating}` === `${rating}` ? {rating: '0'} : {rating: r.rating})}
+                      className={`${r.rating}` === `${rating}` ? 'active' : ''}
+                    >
+                      <Rating caption={' & up'} rating={r.rating}></Rating>
+                    </Link>
+                  </li>
+
+                ))}
+              </ul>
+            </div>
+            <button className="primary smallscreens-filters-close-btn" onClick={() => setIsFiltersOpen(false)}>CLOSE</button>
+          </div>
+        </div>
+
         <div className="col-3">
 
         <div className="row top">
