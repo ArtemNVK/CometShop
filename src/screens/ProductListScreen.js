bdd1f1dfe916ca7894fff93d04a97d783ffabad8
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-06-01 06:25:34 +0300

    Fix products and users admin tables

--- src/screens/ProductListScreen.js
+++ src/screens/ProductListScreen.js
@@ -56,6 +56,7 @@ export default function ProductListScreen(props) {
 
   return (
     <div>
+      <div className="createproduct-btn-container">
        <button 
         type="button" 
         className="primary smallscreens-createproduct-btn"  
@@ -65,6 +66,7 @@ export default function ProductListScreen(props) {
         >
           Create Product
         </button>
+      </div>
       
 
       {loadingDelete && <LoadingBox></LoadingBox>}
