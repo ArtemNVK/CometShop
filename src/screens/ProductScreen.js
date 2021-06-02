1552cbfa350968345b72e1e6cd37e1ed6621093c
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-06-02 16:32:20 +0300

    Fix img zoom

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -90,22 +90,22 @@ function imageZoom(imgID){
 
 	function moveLens(){
 		let pos = getCursor()
-		let positionLeft = pos.x - (lens.offsetWidth / 2)
-		let positionTop = pos.y - (lens.offsetHeight / 2)
-		if(positionLeft < 0 ){
-			positionLeft = 0
-		}
-		if(positionTop < 0 ){
-			positionTop = 0
-		}
-		if(positionLeft > img.width - lens.offsetWidth /3 ){
-			positionLeft = img.width - lens.offsetWidth /3
-		}
-		if(positionTop > img.height - lens.offsetHeight /3 ){
-			positionTop = img.height - lens.offsetHeight /3
-		}
-		lens.style.left = positionLeft + 'px';
-		lens.style.top = positionTop + 'px';
+		// let positionLeft = pos.x - (lens.offsetWidth / 2)
+		// let positionTop = pos.y - (lens.offsetHeight / 2)
+		// if(positionLeft < 0 ){
+		// 	positionLeft = 0
+		// }
+		// if(positionTop < 0 ){
+		// 	positionTop = 0
+		// }
+		// if(positionLeft > img.width - lens.offsetWidth /3 ){
+		// 	positionLeft = img.width - lens.offsetWidth /3
+		// }
+		// if(positionTop > img.height - lens.offsetHeight /3 ){
+		// 	positionTop = img.height - lens.offsetHeight /3
+		// }
+		// lens.style.left = positionLeft + 'px';
+		// lens.style.top = positionTop + 'px';
 		lens.style.backgroundPosition = "-" + (pos.x * ratio) + 'px -' +  (pos.y * ratio) + 'px'
 	}
 
