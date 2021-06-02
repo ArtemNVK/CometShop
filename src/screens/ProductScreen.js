b84bd84aa0eee01f37575e7a464f538e59b440b0
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-06-02 17:03:44 +0300

    Fix img zoom

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -114,7 +114,7 @@ function imageZoom(imgID){
     let bounds = img.getBoundingClientRect()
 
     let x = e.pageX - bounds.left
-		let y = e.pageY - bound.top
+		let y = e.pageY - bounds.top
 		x = x - window.pageXOffset;
 		y = y - window.pageYOffset;
 		return {'x':x, 'y':y}
