809cfb9a8ea16132b616108fa6ff0f6cddff73c4
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-06-04 22:34:43 +0300

    FIX zoom img

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -82,7 +82,7 @@ function imageZoom(imgID){
 	lens.style.backgroundImage = `url( ${img.src} )`
 
 
-	let ratio = 3
+	let ratio = 1
 
 	// lens.style.backgroundSize = (img.width * ratio) + 'px ' + (img.height * ratio) + 'px';
 
@@ -92,22 +92,22 @@ function imageZoom(imgID){
 
 	function moveLens(){
 		let pos = getCursor()
-		let positionLeft = pos.x - (lens.offsetWidth / 2)
-		let positionTop = pos.y - (lens.offsetHeight / 2)
-		if(positionLeft < 0 ){
-			positionLeft = 0
-		}
-		if(positionTop < 0 ){
-			positionTop = 0
-		}
-		if(positionLeft > img.width - lens.offsetWidth /3 ){
-			positionLeft = img.width - lens.offsetWidth /3
-		}
-		if(positionTop > img.height - lens.offsetHeight /3 ){
-			positionTop = img.height - lens.offsetHeight /3
-		}
-		lens.style.left = positionLeft + 'px';
-		lens.style.top = positionTop + 'px';
+		// let positionLeft = pos.x - (lens.offsetWidth / 2)
+		// let positionTop = pos.y - (lens.offsetHeight / 2)
+		// if(positionLeft < 0 ){
+		// 	positionLeft = 0
+		// }
+		// if(positionTop < 0 ){
+		// 	positionTop = 0
+		// }
+		// if(positionLeft > img.width - lens.offsetWidth /3 ){
+		// 	positionLeft = img.width - lens.offsetWidth /3
+		// }
+		// if(positionTop > img.height - lens.offsetHeight /3 ){
+		// 	positionTop = img.height - lens.offsetHeight /3
+		// }
+		// lens.style.left = positionLeft + 'px';
+		// lens.style.top = positionTop + 'px';
   
 
 		lens.style.backgroundPosition = "-" + (pos.x * ratio) + 'px -' +  (pos.y * ratio) + 'px'
