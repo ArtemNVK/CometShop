9c8f1a1bba7a540394374d734e2761c0219aefc4
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-06-05 01:18:37 +0300

    FIX zoom img

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -117,10 +117,10 @@ function imageZoom(imgID){
 	function getCursor(){
     let e = window.event
     let bounds = img.getBoundingClientRect()
-    let x = e.pageX - bounds.left;
-    let y = e.pageY - bounds.top;
-		x = x - window.pageXOffset - 150;
-		y = y - window.pageYOffset - 150;
+    let x = window.pageXOffset - bounds.left;
+    let y = window.pageYOffset - bounds.top;
+		x = x - e.pageX;
+		y = y - e.pageY;
 		return {'x': x, 'y': y}
 	}
 
