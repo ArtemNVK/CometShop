a39cad6f8a48a6cfa89ffa37deee99a849dc82c0
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-06-04 22:54:45 +0300

    FIX zoom img

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -82,7 +82,7 @@ function imageZoom(imgID){
 	lens.style.backgroundImage = `url( ${img.src} )`
 
 
-	let ratio = 2
+	let ratio = 3
 
 	// lens.style.backgroundSize = (img.width * ratio) + 'px ' + (img.height * ratio) + 'px';
 
