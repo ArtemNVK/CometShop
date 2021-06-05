fca4368e1f340edf265be7fffdc74acfd2f0648b
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-06-06 01:29:02 +0300

    f

--- src/screens/ProductCreateScreen.js
+++ src/screens/ProductCreateScreen.js
@@ -63,12 +63,8 @@ export default function ProductCreateScreen(props) {
   const uploadImgsHandler = async (e) => {
     const files = Array.from(e.target.files);
     const bodyFormData = new FormData();
-    
-    bodyFormData.append('images', files);
-    
-
-    console.log(bodyFormData)
-
+    bodyFormData.append('images', files[0]);
+    console.log(bodyFormData);
     setLoadingImgsUpload(true);
     try {
       const { data } = await Axios.post('https://cometshop.herokuapp.com/api/uploads/imgs', bodyFormData, {
