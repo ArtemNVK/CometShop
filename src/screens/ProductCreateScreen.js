483288e724c2be52b806c12f44fbe5823b6ad22d
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-06-14 02:00:10 +0300

    Add multiple imgs upload SUCCESS

--- src/screens/ProductCreateScreen.js
+++ src/screens/ProductCreateScreen.js
@@ -61,8 +61,7 @@ export default function ProductCreateScreen(props) {
   };
 
   const uploadImgsHandler = async (e) => {
-    const files = e.target.files;
-    // const files = Array.from(e.target.files);
+    const files = Array.from(e.target.files);
     const bodyFormData = new FormData();
     files.forEach(file => {
       bodyFormData.append('images', file);
