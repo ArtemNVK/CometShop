41d1cfbbe9b74c80de5c5ec953a3b02773c442d2
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-06-29 03:11:24 +0300

    Add pagination to where it belongs to

--- src/components/PromotionsCarousel.js
+++ src/components/PromotionsCarousel.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { sliderData } from '../utils'
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
 import { Link } from 'react-router-dom';
 
 export default function PromotionsCarousel({slides}) {
     const [current, setCurrent] = useState(0);
+    const [touchPosition, setTouchPosition] = useState(null)
     const length = slides.length;
 
     if(!Array.isArray(slides) || slides.length <= 0) {
@@ -19,22 +19,46 @@ export default function PromotionsCarousel({slides}) {
         setCurrent(current === 0 ? length - 1 : current - 1)
     }
 
+    // mobile screens
+
+    const handleTouchStart = (e) => {
+        const touchDown = e.touches[0].clientX
+        setTouchPosition(touchDown)
+    }
+
+    const handleTouchMove = (e) => {
+        const touchDown = touchPosition
+    
+        if(touchDown === null) {
+            return
+        }
+    
+        const currentTouch = e.touches[0].clientX
+        const diff = touchDown - currentTouch
+    
+        if (diff > 5) {
+            nextSlide()
+        }
+    
+        if (diff < -5) {
+            prevSlide()
+        }
+    
+        setTouchPosition(null)
+    }
+
     return (
         <section className="slider">
             <i class="fa fa-arrow-left arrow left" onClick={prevSlide}></i>
             <i class="fa fa-arrow-right arrow right" onClick={nextSlide}></i>
-            {/* <FaArrowAltCircleLeft 
-                className="arrow left" 
-                onClick={prevSlide}/>
-            <FaArrowAltCircleRight
-                className="arrow right" 
-                onClick={nextSlide}/> */}
             {sliderData.map((slide, index) => {
                 return (
                     
                     <div 
                         className={index === current ? 'slide active' : 'slide'}
                         key={index}
+                        onTouchStart={handleTouchStart}
+                        onTouchMove={handleTouchMove}
                     >
                         {index === current && (
                         <Link to={slide.category !== null ? `/search/category/${slide.category}` : "#"}>
